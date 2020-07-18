26a27d5c38ce0fa02c1cd82e124eec160443f835
Joshua Draper <josh.drapergame@gmail.com>
2020-07-18 09:16:00 -0600

    finished histo.py

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,30 @@
 # Your code here
 
+with open('applications/histo/robin.txt') as f:
+    words = f.read()
+
+    ignore = (":", ";", ",", ".", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&", "\"", "?", "!")
+
+    for char in ignore:
+        words = words.replace(char, "")
+
+    word_split = words.lower().split()
+
+
+    cache = {}
+
+
+
+
+    for word in word_split:
+        if word not in cache:
+            cache[word] = '#'
+        else:
+            cache[word] += '#'
+
+    items = list(cache.items())
+
+    items.sort(key=lambda x: x[1], reverse=True)
+
+    for key, value in items:
+        print(f'{key:<18} {value}')
