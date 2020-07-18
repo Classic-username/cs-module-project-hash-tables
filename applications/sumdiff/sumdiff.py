d515789a84ceb1614d9e01700c4ad7c879c162ca
Joshua Draper <josh.drapergame@gmail.com>
2020-07-18 10:23:29 -0600

    Finished sumdiff

--- applications/sumdiff/sumdiff.py
+++ applications/sumdiff/sumdiff.py
@@ -2,6 +2,7 @@
 find all a, b, c, d in q such that
 f(a) + f(b) = f(c) - f(d)
 """
+import random
 
 #q = set(range(1, 10))
 #q = set(range(1, 200))
@@ -12,4 +13,26 @@ def f(x):
     return x * 4 + 6
 
 # Your code here
+
+
+cache1 = {}
+cache2 = {}
+
+for i in range(0, len(q)):
+    for j in range(0, len(q)):
+        cache1[f'f({q[i]}) + f({q[j]})'] = f(q[i]) + f(q[j])
+        cache2[f'f({q[j]}) - f({q[i]})'] = f(q[j]) - f(q[i])
+
+for fromAdd in cache1:
+    for fromSub in cache2:
+        if cache2[fromSub] == cache1[fromAdd]:
+            print(f'{fromAdd} = {fromSub} = {cache1[fromAdd]}')
+
+
+
+
+# a = random.choice(s)
+# b = random.choice(s)
+# c = random.choice(s)
+# d = random.choice(s)
 
