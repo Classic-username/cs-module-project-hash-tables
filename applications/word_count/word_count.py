458056aaf486bd856adc2110b4d64b1dc3ff95ed
Joshua Draper <josh.drapergame@gmail.com>
2020-07-18 09:32:04 -0600

    finished word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,21 @@
 def word_count(s):
     # Your code here
 
+    cache = {}
+
+    ignore = (":", ";", ",", ".", "-", "+", "=", "/", "\\", "|", "[", "]", "{", "}", "(", ")", "*", "^", "&", "\"")
+
+
+    for char in ignore:
+       s = s.replace(char, "")
+    s = s.lower().split()
+    for word in s:
+        if word not in cache:
+            cache[word] = 1
+        else:
+            cache[word] += 1
+
+    return cache
 
 
 if __name__ == "__main__":
@@ -8,3 +23,4 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+
